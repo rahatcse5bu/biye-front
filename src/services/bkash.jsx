7889b8c63450d37d724e1db571f9a430c11cc213
anis009 <anis.cse5.bu@gmail.com>
2024-06-29 20:38:29 +0600

    trying to fix payment issue

--- src/services/bkash.jsx
+++ src/services/bkash.jsx
@@ -1,81 +1,81 @@
 import axios from "axios";
 const baseUrl =
-	import.meta.env.VITE_REACT_APP_NODE_ENV === "development"
-		? "http://localhost:5000/api/v1"
-		: "https://server.pnc-nikah.com/api/v1";
+  import.meta.env.VITE_REACT_APP_NODE_ENV === "development"
+    ? "http://localhost:5000/api/v1"
+    : "https://server.pnc-nikah.com/api/v1";
 
 export default function BkashCreatePaymentAPICall(amount, bioId = 0) {
-	console.log("Button Clicked !!");
-	let url = `https://pnc-nikah.com/pay${bioId > 0 ? `?bioId=${bioId}` : ""}`;
-	// console.log(url, bioId);
-	axios
-		.post(baseUrl + "/bkash/create", {
-			amount: amount,
-			callbackURL: url,
-		})
-		.then((response) => {
-			console.log("Data was successfully sent.", response);
-			// console.log(response);
-			if (response?.data?.bkashURL) {
-				window.location.href = response?.data?.bkashURL;
-			} else {
-				window.location.href = "/";
-			}
-		})
-		.catch((error) => {
-			console.log("An error occurred:", error);
-		});
+  console.log("Button Clicked !!");
+  let url = `https://pnc-nikah.com/pay${bioId > 0 ? `?bioId=${bioId}` : ""}`;
+  // console.log(url, bioId);
+  axios
+    .post(baseUrl + "/bkash/create", {
+      amount: amount,
+      callbackURL: url,
+    })
+    .then((response) => {
+      // console.log("Data was successfully sent.", response);
+      // console.log(response);
+      if (response?.data?.bkashURL) {
+        window.location.href = response?.data?.bkashURL;
+      } else {
+        window.location.href = "/";
+      }
+    })
+    .catch((error) => {
+      console.log("An error occurred:", error);
+    });
 }
 export function BkashExecutePaymentAPICall(paymentID) {
-	return new Promise((resolve, reject) => {
-		axios
-			.post(baseUrl + "/bkash/execute", {
-				paymentID: paymentID,
-			})
-			.then((response) => {
-				// console.log('Data was successfully sent.', response.data);
-				resolve(response.data); // Resolve the promise with the response data
-			})
-			.catch((error) => {
-				console.log("An error occurred:", error);
-				reject(error); // Reject the promise with the error
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .post(baseUrl + "/bkash/execute", {
+        paymentID: paymentID,
+      })
+      .then((response) => {
+        // console.log('Data was successfully sent.', response.data);
+        resolve(response.data); // Resolve the promise with the response data
+      })
+      .catch((error) => {
+        console.log("An error occurred:", error);
+        reject(error); // Reject the promise with the error
+      });
+  });
 }
 
 export function BkashQueryPaymentAPICall(paymentID) {
-	return new Promise((resolve, reject) => {
-		axios
-			.post(baseUrl + "/bkash/query", {
-				paymentID: paymentID,
-			})
-			.then((response) => {
-				//console.log('Data was successfully sent.', response.data);
-				resolve(response.data); // Resolve the promise with the response data
-			})
-			.catch((error) => {
-				console.log("An error occurred:", error);
-				reject(error); // Reject the promise with the error
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .post(baseUrl + "/bkash/query", {
+        paymentID: paymentID,
+      })
+      .then((response) => {
+        //console.log('Data was successfully sent.', response.data);
+        resolve(response.data); // Resolve the promise with the response data
+      })
+      .catch((error) => {
+        console.log("An error occurred:", error);
+        reject(error); // Reject the promise with the error
+      });
+  });
 }
 
 export function BkashRefundPaymentAPICall(paymentID, trxID, amount) {
-	return new Promise((resolve, reject) => {
-		axios
-			.post(baseUrl + "/bkash/refund", {
-				paymentID: paymentID,
-				trxID: trxID,
-				amount: amount,
-				sku: "test",
-				reason: "test",
-			})
-			.then((response) => {
-				resolve(response.data);
-			})
-			.catch((error) => {
-				console.log("An error occurred:", error);
-				reject(error);
-			});
-	});
+  return new Promise((resolve, reject) => {
+    axios
+      .post(baseUrl + "/bkash/refund", {
+        paymentID: paymentID,
+        trxID: trxID,
+        amount: amount,
+        sku: "test",
+        reason: "test",
+      })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        console.log("An error occurred:", error);
+        reject(error);
+      });
+  });
 }
