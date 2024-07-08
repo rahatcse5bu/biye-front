749f8f398575e8c9b0abeaab36d0c035c5e85212
Md. Rahat <rahat.cse5.bu@gmail.com>
2024-07-08 08:40:19 +0600

    Refund Policy added to header

--- src/pages/RefundPolicy/RefundPolicy.jsx
+++ src/pages/RefundPolicy/RefundPolicy.jsx
@@ -1,68 +1,46 @@
 // import React from 'react';
 import { Colors } from "../../constants/colors";
 function RefundPolicy() {
-	return (
-		<div className="min-h-screen bg-gray-100 p-8">
-			<div className="max-w-screen-md mx-auto bg-white p-6 rounded-lg shadow-md">
-				<h1
-					className="text-xl md:text-2xl lg:text-3xl font-semibold text-center mb-8"
-					style={{ color: Colors.titleText }}
-				>
-					রিফান্ড পলিসি
-				</h1>
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <div className="max-w-screen-md mx-auto bg-white p-6 rounded-lg shadow-md">
+        <h1
+          className="text-xl md:text-2xl lg:text-3xl font-semibold text-center mb-8"
+          style={{ color: Colors.titleText }}
+        >
+          রিফান্ড পলিসি
+        </h1>
 
-				<p className="text-gray-700 mb-6">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec lorem
-					eu nisi cursus convallis ac eu massa. Nullam scelerisque mi ac
-					aliquam. Nullam ultrices venenatis tristique.
-				</p>
+        <p className="text-gray-700 mb-6">
+          আমি কি রিফান্ড রিকুয়েস্ট করতে পারবো? জ্বি। আপনি আপনার একাউন্টে
+          পর্যাপ্ত পয়েন্ট থাকা সাপেক্ষে রিফান্ড রিকুয়েস্ট করতে পারবেন।
+        </p>
 
-				<p className="text-gray-700 mb-6">
-					Sed sed velit vel sapien dignissim bibendum a at orci. In hac
-					habitasse platea dictumst. Sed tincidunt odio sed ante facilisis, id
-					cursus sapien ullamcorper. Proin id metus quis nulla sagittis
-					ultricies in ac metus.
-				</p>
+        <p className="text-gray-700 mb-6">
+          রিফান্ড পলিসি কিরকম? আপনার একাউন্টে পর্যাপ্ত পয়েন্ট থাকা সাপেক্ষে শুধু
+          রিফান্ড রিকুয়েস্ট করতে পারবেন! পেমেন্ট করার ৬ ঘন্টার মধ্যে রিফান্ড
+          রিকুয়েস্ট করলে স্বাভাবিক রিফান্ড পাবেন ইনশাআল্লহ। পেমেন্ট করার ৬ ঘন্টা
+          পর কিংবা ৩ দিনের মধ্যে রিফান্ড রিকুয়েস্ট করলে ১.৫ পয়েন্ট=১৳ রেটে
+          রিফান্ড পাবেন।
+        </p>
 
-				<p className="text-gray-700 mb-6">
-					Phasellus eu odio bibendum, tincidunt erat vel, bibendum orci. Proin
-					bibendum quam non enim facilisis, at posuere felis aliquam. Vivamus
-					sollicitudin tellus eu erat hendrerit, a condimentum est suscipit.
-				</p>
+        <p className="text-gray-700 mb-6">
+          কতদিন পর্যন্ত রিফান্ড রিকুয়েস্ট করতে পারবো? ৩ দিন পর্যন্ত রিফান্ড
+          রিকুয়েস্ট করতে পারবেন।
+        </p>
 
-				<p className="text-gray-700 mb-6">
-					Integer aliquam ligula ut diam fermentum, nec aliquam nulla dictum.
-					Sed malesuada est a justo rhoncus, et efficitur justo egestas. Nulla
-					nec diam ut velit tincidunt bibendum.
-				</p>
+        <p className="text-gray-700 mb-6">
+          কাস্টম এমাউন্ট রিফান্ড করতে পারবো? না, পারবেন না। আপনার করা
+          ট্রান্সজেকশনের বিপরীতে শুধুমাত্র রিফান্ড রিকুয়েস্ট করতে পারবেন।
+        </p>
 
-				<p className="text-gray-700 mb-6">
-					Sed congue tortor in scelerisque vehicula. Nullam eget felis ultrices,
-					finibus lorem id, convallis dui. Vestibulum bibendum libero et lacus
-					facilisis, eget tincidunt ipsum bibendum.
-				</p>
-
-				<p className="text-gray-700 mb-6">
-					Proin rhoncus orci sit amet dui posuere, id sollicitudin dui lacinia.
-					Vivamus laoreet felis id ex tincidunt, at fermentum urna lobortis.
-					Quisque euismod metus a libero vulputate venenatis.
-				</p>
-
-				<p className="text-gray-700 mb-6">
-					Nunc euismod sapien vel hendrerit euismod. Vivamus id nulla id est
-					auctor dapibus. Duis et arcu vel justo efficitur malesuada. Sed
-					ultrices ex at risus vulputate, non finibus justo posuere.
-				</p>
-
-				<p className="text-gray-700">
-					Nulla facilisi. Phasellus id nisi quis erat sollicitudin iaculis. Ut
-					ut velit id elit volutpat varius. Donec id erat in orci fringilla
-					faucibus. Proin tincidunt turpis et justo vestibulum, vel malesuada
-					purus aliquet.
-				</p>
-			</div>
-		</div>
-	);
+        <p className="text-gray-700 mb-6">
+          রিফান্ড পেতে কতদিন লাগতে পারে? নির্ভর করে। সর্বোচ্চ ৩ কার্যদিবসের
+          মধ্যে রিফান্ড পাবেন।
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default RefundPolicy;
