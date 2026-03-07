193d0c09717e237cd181f81dc9f960de9a41c5d7
Md. Rahat <rahat.cse5.bu@gmail.com>
2026-03-07 17:29:15 +0600

    menu nav dropdown position in  mobile+toggle fixed

--- src/components/Header/Sublinks.jsx
+++ src/components/Header/Sublinks.jsx
@@ -1,22 +1,28 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import DrobdownIcon from '../../assets/icons/Dropdown.jsx';
 
 const SubLinks = ({ navItem, setOpenNav }) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <li className="group relative inline-block  antialiased font-sans text-white text-lg font-semibold nav-item-primary nav-item-dropdown">
-      <button className="flex  items-center   ease-linear transition-all duration-150 h-full">
+    <li className="group relative block lg:inline-block  antialiased font-sans text-white text-lg font-semibold nav-item-primary nav-item-dropdown">
+      <button
+        className="flex items-center justify-center w-full lg:w-auto ease-linear transition-all duration-150 h-full"
+        onClick={() => setOpen(!open)}
+      >
         {navItem.title} <DrobdownIcon />{' '}
       </button>
-      <ul className="absolute left-0 lg:left-0 hidden text-gray-700 group-hover:block w-full lg:w-40 text-sm nav-item-dropdown-ul">
+      <ul className={`absolute left-0 text-gray-700 w-full lg:w-40 text-sm nav-item-dropdown-ul ${open ? 'block' : 'hidden'} lg:hidden lg:group-hover:block`}>
         {navItem.subLinks.map((_linkItem, _in) =>
           _linkItem.path === '/voter-list' ? (
             <a
               key={_in}
               href="http://66.29.130.89:4001/notices/63d147b70bfbc8c31261a39d.pdf"
               className="whitespace-no-wrap block py-2 px-4 hover:bg-gray-400"
+              onClick={() => { setOpen(false); setOpenNav(false); }}
             >
               {_linkItem.title}
             </a>
@@ -25,7 +31,7 @@ const SubLinks = ({ navItem, setOpenNav }) => {
               key={_in}
               to={_linkItem.path}
               className="whitespace-no-wrap block py-2 px-4 hover:bg-gray-400"
-              onClick={() => setOpenNav(false)}
+              onClick={() => { setOpen(false); setOpenNav(false); }}
             >
               {_linkItem.title}
             </Link>
